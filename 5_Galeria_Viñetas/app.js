e8108937a6daf961e9fd39b2903e0983e443284e
Sebas Meza <xdjuasjuan@gmail.com>
2025-08-26 15:56:29 -0500

    Add files via upload
    Audios agregados

--- 5_Galeria_Viñetas/app.js
+++ 5_Galeria_Viñetas/app.js
@@ -28,7 +28,6 @@ corazones.forEach(function (corazon) {
     })
 })
 
-
 const escenas = document.querySelectorAll('.escena')
 const btnAnt = document.querySelector('.anterior')
 const btnSig = document.querySelector('.siguiente')
@@ -48,6 +47,9 @@ btnSig.addEventListener('click', function(){
     if (indice >= escenas.length){
         indice = 0
     }
+    ostOne.pause()
+    ostTwo.pause()
+    ostThree.pause()
     mostrarEsc(indice)
 })
 
@@ -56,5 +58,46 @@ btnAnt.addEventListener('click', function(){
     if (indice < 0){
         indice = escenas.length - 1
     }
+    ostOne.pause()
+    ostTwo.pause()
+    ostThree.pause()
     mostrarEsc(indice)
+})
+
+miniaturas.forEach( (miniatura, i) => {
+    miniatura.addEventListener('click', function(){
+        mostrarEsc(i)
+    })
+});
+
+const ostOne = new Audio('./assets/ost.mp3')
+const ostTwo = new Audio('./assets/ost2.mp3')
+const ostThree = new Audio('./assets/ost3.mp3')
+
+let playOne = document.querySelector('#play')
+let playTwo = document.querySelector('#play2')
+let playThree = document.querySelector('#play3')
+
+playOne.addEventListener('click', function(){
+    ostOne.play()
+})
+playTwo.addEventListener('click', function(){
+    ostTwo.play()
+})
+playThree.addEventListener('click', function(){
+    ostThree.play()
+})
+
+let stopOne = document.querySelector('#stop')
+let stopTwo = document.querySelector('#stop2')
+let stopThree = document.querySelector('#stop3')
+
+stopOne.addEventListener('click', function(){
+    ostOne.pause()
+})
+stopTwo.addEventListener('click', function(){
+    ostTwo.pause()
+})
+stopThree.addEventListener('click', function(){
+    ostThree.pause()
 })
